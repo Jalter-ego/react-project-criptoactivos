880357595c309b4c0fd61d48641be7dee9430c5f
jalter-ego <jhademels123@gmail.com>
2025-09-23 15:02:10 -0400

    feat: added page dashboard

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            
+            <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
     )
 }
