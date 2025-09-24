172b0aee236eb3089ba956438a153fe1ab084140
jalter-ego <jhademels123@gmail.com>
2025-09-24 16:23:18 -0400

    feat: the settings page was added

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
+import SettingsPage from "./pages/Settings";
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
         </Routes>
     )
 }
