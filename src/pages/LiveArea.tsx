2d59a9189c2aa7e5985bf19da88fcc3250449ae1
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-09-24 18:45:51 -0400

    charts view

--- src/pages/LiveArea.tsx
+++ src/pages/LiveArea.tsx
@@ -6,8 +6,7 @@ export default function LiveAreaPage() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-gray-900 text-white">
       <h1 className="text-2xl font-bold my-4">Live Area Chart </h1>
-      <div className="w-full max-w-4xl">
-        <TradingChart />
+      <div className="w-full max-w-4xl">        <TradingChart />
       </div>
     </div>
   );
