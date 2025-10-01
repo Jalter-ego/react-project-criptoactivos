ea71c633ce35fab05cd137c8a5e0954a005f8409
jalter-ego <jhademels123@gmail.com>
2025-10-01 17:15:40 -0400

    feat: update context portafolio

--- src/hooks/PortafolioContext.tsx
+++ src/hooks/PortafolioContext.tsx
@@ -1,23 +1,23 @@
 import React, { createContext, useContext, useState } from "react";
-import type { Portafolio } from "@/services/portafolioServices";
+import type { PortafolioWithHoldings } from "@/services/portafolioServices";
 
 type PortafolioContextType = {
-  currentPortafolio: Portafolio | null;
-  setCurrentPortafolio: (p: Portafolio) => void;
+  currentPortafolio: PortafolioWithHoldings | null;
+  setCurrentPortafolio: (p: PortafolioWithHoldings) => void;
   clearPortafolio: () => void;
 };
 
 const PortafolioContext = createContext<PortafolioContextType | undefined>(undefined);
 
 export const PortafolioProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [currentPortafolio, setCurrentPortafolioState] = useState<Portafolio | null>(() => {
+  const [currentPortafolio, setCurrentPortafolioState] = useState<PortafolioWithHoldings | null>(() => {
     // Cargar desde localStorage al iniciar
     const saved = localStorage.getItem("currentPortafolio");
-    return saved ? JSON.parse(saved) as Portafolio : null;
+    return saved ? JSON.parse(saved) as PortafolioWithHoldings : null;
   });
 
 
-  const setCurrentPortafolio = (p: Portafolio) => {
+  const setCurrentPortafolio = (p: PortafolioWithHoldings) => {
     setCurrentPortafolioState(p);
     localStorage.setItem("currentPortafolio", JSON.stringify(p));
   };
