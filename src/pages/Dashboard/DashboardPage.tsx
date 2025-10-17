30e5f408ca978d6c1d8345d6291ec07bebe3c2f1
jalter-ego <jhademels123@gmail.com>
2025-10-17 12:17:16 -0400

    feat: testing for chart

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import CardHeaders from "@/components/Dashboard/CardHeaders";
 import CardListActives from "@/components/Dashboard/CardListActives";
+import PerformanceChart from "@/components/Dashboard/PerformanceChart";
 import TransactionsHistory from "@/components/TradePage/TransactionsHistory";
 import { Input } from "@/components/ui/input";
 import { usePortafolio } from "@/hooks/PortafolioContext";
@@ -14,17 +15,17 @@ export default function DashboardPage() {
     const { currentPortafolio } = usePortafolio();
     const [transactions, setTransactions] = useState<Transaction[]>([])
 
-    useEffect (()=>{
+    useEffect(() => {
         if (currentPortafolio) {
             transactionServices.findAllByPortafolio(currentPortafolio.id)
-            .then(setTransactions)
-            .catch(()=> toast("No se pudo obtener las transacciones"))
+                .then(setTransactions)
+                .catch(() => toast("No se pudo obtener las transacciones"))
         }
-    },[])
+    }, [])
     return (
         <Layout>
             <div className="w-full flex flex-col">
-                <CardHeaders/>
+                <CardHeaders />
                 <section className="w-full grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
                     <div className="lg:col-span-2 h-fit bg-card rounded-md p-4 border shadow-lg">
                         <div className="border-b pb-4">
@@ -35,8 +36,11 @@ export default function DashboardPage() {
                         <TransactionsHistory
                             transactions={transactions}
                         />
+                        <div className="mt-6">
+                            <PerformanceChart portafolioId={currentPortafolio?.id || ''} />
+                        </div>
                     </div>
-                    <CardListActives/>
+                    <CardListActives />
                 </section>
             </div>
         </Layout>
