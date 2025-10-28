a0592061fd228cd7ceb15fd11ba5fd0becbee139
jalter-ego <jhademels123@gmail.com>
2025-10-28 15:46:53 -0400

    feat: delete title for liveArea

--- src/pages/LiveArea.tsx
+++ src/pages/LiveArea.tsx
@@ -5,7 +5,6 @@ export default function LiveAreaPage() {
   const { symbol } = useParams<{ symbol: string }>();
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-white-900 text-black">
-      <h1 className="text-3xl font-bold my-8">Live Area Chart</h1>
       <div className="w-full flex justify-center">
         <div className="w-full max-w-7xl h-[80vh] flex items-center justify-center">
           <div className="w-full h-full rounded-xl shadow-2xl overflow-hidden bg-gray-800">
